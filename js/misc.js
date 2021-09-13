c62f316ed0cb54bfa7505248deed462f84ec3561
steinnhauser <hausersteinn@gmail.com>
2021-09-13 09:53:49 +0200

    Completed topnav, fixed image spacing and p tags

--- js/misc.js
+++ js/misc.js
@@ -1,34 +1,20 @@
-function triggerDropdown() {
-  // Show the dropdown menu or hide it when clicked.
-  const el = document.getElementById("dropdown-menu");
-  console.log(el.style.display);
-  if (el.style.display === "none") {
-    el.style.display = "block"; //"flex";
-  } else {
-    // hide the window again
-    el.style.display = "none";
-  }
-}
-
-
-
-
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-// function myFunction() {
-//   document.getElementById("myDropdown").classList.toggle("show");
-// }
-//
-// // Close the dropdown menu if the user clicks outside of it
-// window.onclick = function(event) {
-//   if (!event.target.matches('.dropbtn')) {
-//     var dropdowns = document.getElementsByClassName("dropdown-content");
-//     var i;
-//     for (i = 0; i < dropdowns.length; i++) {
-//       var openDropdown = dropdowns[i];
-//       if (openDropdown.classList.contains('show')) {
-//         openDropdown.classList.remove('show');
-//       }
-//     }
-//   }
-// }
+function triggerDropdown() {
+  document.getElementById("dropdown-menu").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  var toggleTarget = '.triple-choice-bar'; //'.dropbtn';
+  if (!event.target.matches(toggleTarget)) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
