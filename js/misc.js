bc4a2fc0b9c6cb9821d409097c6a8ef67bd11f6b
steinnhauser <hausersteinn@gmail.com>
2021-09-15 14:32:12 +0200

    Fixed toggle issue in topnav

--- js/misc.js
+++ js/misc.js
@@ -1,15 +1,36 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function triggerDropdown() {
-  document.getElementById("dropdown-menu").classList.toggle("show");
+  console.log('Triggering dropdown...');
+  var elm = document.getElementById("dropdown-menu");
+  console.log(elm);
+  elm.classList.toggle("show");
+
+  // if (elm.classList.contains('show')) {
+  //   elm.classList.remove('show');
+  // } else {
+  //   elm.classList.toggle("show");
+  // }
 }
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
 
-  var toggleTarget = '.triple-choice-bar'; //'.dropbtn';
-  if (!event.target.matches(toggleTarget)) {
+  console.log(event.target);
 
+  var toggleTarget = '.triple-choice-bar';
+  var targetMatches = event.target.matches(toggleTarget);
+
+  // check for the three additional divs hidden in the button
+  var i;
+  for (i = 1; i < 4; i++) {
+    var additionalDiv = `${toggleTarget}-${i}`;
+    var matchAdditionalDivs = event.target.matches(additionalDiv);
+    targetMatches = (targetMatches || matchAdditionalDivs);
+  }
+
+  // if the click is outside of these regions, close the dropdown content
+  if (!targetMatches) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
