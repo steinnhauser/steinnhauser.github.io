57741b3a3aa7173c005a439e3457c0df865506b6
steinnhauser <hausersteinn@gmail.com>
2022-08-15 10:59:07 +0200

    Fix abstract to button, not container elem. Consider refactoring function to distinguish between siblings and children.

--- js/collapsibles.js
+++ js/collapsibles.js
@@ -1,7 +1,11 @@
 
 function triggerCollapsible(clicked_obj) {
     var jquery_object = jQuery(clicked_obj);
-    var collapsible_elems = jquery_object.children().filter(function () {return $(this).hasClass("collapsible");})
+
+    // search for collapsible divs in both children and siblings
+    var collapsible_elems_kids = jquery_object.children().filter(function () {return $(this).hasClass("collapsible");})
+    var collapsible_elems_sibs = jquery_object.siblings().filter(function () {return $(this).hasClass("collapsible");})
+    var collapsible_elems = jQuery.merge(collapsible_elems_kids, collapsible_elems_sibs);
     var was_closed = collapsible_elems.is(':hidden'); // extract if it was closed before or not
     
     // collapse the element
@@ -11,7 +15,9 @@ function triggerCollapsible(clicked_obj) {
 
     // give fade arrows (if there are any) "active" status.
     // var arrows = jquery_object.children().filter(function () {return $(this).hasClass("fade-arrow");});
-    var arrows = jquery_object.children().find("i").filter(function () {return $(this).hasClass("down-hat")});
+    var arrows_kids = jquery_object.children().find("i").filter(function () {return $(this).hasClass("down-hat")});
+    var arrows_sibs = jquery_object.siblings().find("i").filter(function () {return $(this).hasClass("down-hat")});
+    var arrows = jQuery.merge(arrows_kids, arrows_sibs);
     if (arrows.length > 0)
         if (was_closed)
             arrows.addClass("down-hat-active");
